268007b5b4b08d776acc02ee03855edf8669ce19
kcils360 <kcils360@gmail.com>
2020-04-13 23:04:01 -0700

    can read dir and push to title arr

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 'use strict'
 
+//bring in packages
 require('dotenv').config;
+const fs = require('fs');
 const express = require('express');
 const superagent = require('superagent');
 
+//set up the server
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+//Read in the movie dir and put titles in an array
+let file = 'W:\Movies';
 
-app.listen(PORT, ()=> console.log(`Server up on ${PORT}`));
+let mov = [];
+let data = fs.readdirSync(file);
+console.log('data', data);
+data.forEach(element => {
+    mov.push(element.slice(0, -4).toString());
+});
+
+console.log('mov', mov);
+
+//superagent to theMovieDB api to get info about each title and store in array of objects
+//release_date
+//title
+//
+
+//start the server and listen for requests
+// app.listen(PORT, ()=> console.log(`Server up on ${PORT}`));
