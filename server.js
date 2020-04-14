36181ce01c6cda6a979245fda7fd8b29e8030f6d
kcils360 <kcils360@gmail.com>
2020-04-14 13:36:42 -0700

    iterate over the mov array and get movie info from themoviedb api

--- server.js
+++ server.js
@@ -22,6 +22,24 @@ data.forEach(element => {
 
 console.log('mov', mov);
 
+//iterate over the mov array and get movie info from themoviedb api
+let collection = [];
+
+mov.forEach((film, idx) => {
+    let name = film;
+    let URL = `https://api.themoviedb.org/3/search/movie?api_key=d0be1622c33949eba10b6bc5729d9363&language=en-US&query=${name}&page=1&include_adult=false`;
+    
+    superagent.get(URL)
+    .then(data => {
+        let Movie = {};        
+        Movie.title = data.body.results[0].title;
+        Movie.id = data.body.results[0].id;
+        collection.push(Movie);
+        console.log(`collection@${idx}`, collection);
+    })
+})
+
+
 //superagent to theMovieDB api to get info about each title and store in array of objects
 //release_date
 //id - this is the tmdb id to getcredits
@@ -30,7 +48,8 @@ console.log('mov', mov);
 //genre_ids
 //poster_path https://image.tmdb.org/t/p/w500/ +
 ////getcredits - need id
-//
+//filter for job:directing
+//           job:acting
 
 //start the server and listen for requests
 // app.listen(PORT, ()=> console.log(`Server up on ${PORT}`));
